5cc3b0f40e406ffd7526a05401e410e7cba5058d
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2025-09-09 12:02:41 +0200

    feat: create mock login page
    create a mock version of the login page which still lacks functionality and styling
    
    Refs: #14

---
+++ labman/src/app/inventory/page.tsx
@@ -0,0 +1,41 @@
+﻿import prisma from '@/lib/prisma';
+
+export default async function Inventory() {
+    const equipment = await prisma.equipment.findMany();
+    console.log(equipment);
+
+    async function addEquipment(formData : FormData) {
+        "use server"
+        const name = formData.get("name") as string;
+        const category = formData.get("category") as string;
+        const image = formData.get("image") as string;
+        const quantity = formData.get("quantity");
+        const available = formData.get("available");
+
+        await prisma.equipment.create({
+            data: {
+                name,
+                category,
+                image,
+                quantity: Number(quantity),
+                available: Number(available)
+            }
+        });
+
+    }
+    return (
+        <div>
+            <h1>{equipment[0].name}</h1>
+            <form action={addEquipment}>
+                <input type="text" name="name" placeholder="Name" />
+                <input type="text" name="category" placeholder="Category" />
+                <input type="text" name="image" placeholder="Image" />
+                <input type="number" name="quantity" placeholder="Quantity" />
+                <input type="number" name="available" placeholder="Available" />
+                <button type="submit">Add Equipment</button>
+
+            </form>
+        </div>
+
+    );
+}
--- labman/src/app/page.tsx
+++ labman/src/app/page.tsx
@@ -1,41 +1,21 @@
 import prisma from '@/lib/prisma';
+import "./globals.css";
 
 export default async function Home() {
-  const equipment = await prisma.equipment.findMany();
-  console.log(equipment);
-
-  async function addEquipment(formData : FormData) {
-      "use server"
-      const name = formData.get("name") as string;
-      const category = formData.get("category") as string;
-      const image = formData.get("image") as string;
-      const quantity = formData.get("quantity");
-      const available = formData.get("available");
-
-        await prisma.equipment.create({
-            data: {
-                name,
-                category,
-                image,
-                quantity: Number(quantity),
-                available: Number(available)
-            }
-        });
-
-  }
   return (
-      <div>
-        <h1>{equipment[0].name}</h1>
-        <form action={addEquipment}>
-            <input type="text" name="name" placeholder="Name" />
-            <input type="text" name="category" placeholder="Category" />
-            <input type="text" name="image" placeholder="Image" />
-            <input type="number" name="quantity" placeholder="Quantity" />
-            <input type="number" name="available" placeholder="Available" />
-            <button type="submit">Add Equipment</button>
+      <div className="mx-auto w-fit">
+        <h1 className="mx-auto w-fit font-bold text-6xl my-8">VR Lab Management</h1>
+        <div className="my-80">
+          <h1 className="font-bold text-5xl mx-auto w-fit">Login</h1>
+          <form className="flex flex-col gap-5 pt-4">
+            <input type="text" name="username" placeholder="Username" className="bg-white rounded-md p-2 m-2 placeholder-black text-black" />
+            <input type="password" name="password" placeholder="Password" className="bg-white rounded-md p-2 m-2 placeholder-black text-black" />
+            <button type="submit" className="bg-blue-500 text-white rounded-md p-2 m-2">Login</button>
+          </form>
+        </div>
 
-        </form>
       </div>
+
 
   );
 }
