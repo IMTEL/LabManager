0d5950c8632cc50d946bb3b0d701254cc0eeb5af
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2025-10-08 11:56:44 +0200

    feat: display page title
    make a component which gets the pathname and displays as the title in the navbar
    
    Refs: #26 #27

---
+++ labman/src/components/core/PathName.tsx
@@ -0,0 +1,16 @@
+﻿"use client"
+import { usePathname } from "next/navigation";
+
+export default function PathName() {
+    let pathname = usePathname().split("/")[1];
+
+    if (pathname == "") {
+        pathname = "Inventory";
+    } else {
+        pathname = pathname.charAt(0).toUpperCase() + pathname.slice(1);
+    }
+
+    return(
+        <h1 className="pl-4 pt-4 pb-2 font-bold text-5xl">{pathname}</h1>
+    )
+}
