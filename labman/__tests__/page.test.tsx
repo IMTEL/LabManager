66d5b567dd3a921c769fd5691880de365cda695c
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2026-01-09 15:14:25 +0100

    feat: Configure vitest
    install and configure the vitest testing framework for adding unit tests
    
    Refs: #42

---
+++ labman/__tests__/page.test.tsx
@@ -0,0 +1,8 @@
+﻿import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Page from '../src/app/(auth)/login/page'
+
+test('Page', () => {
+    render(<Page />)
+    expect(screen.getByRole('heading', { level: 1, name: 'VR Lab Management' })).toBeDefined()
+})
