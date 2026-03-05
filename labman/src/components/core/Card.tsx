10456a39632d622353c0e966e649eb9862782702
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2026-03-05 17:04:44 +0100

    feat: more ui improvements
    Refs: #52

--- labman/src/components/core/Card.tsx
+++ labman/src/components/core/Card.tsx
@@ -33,9 +33,9 @@ export default function Card({ loan, user, setSelectedLoanId, setSideView }: Car
     }
 
     return(
-        <div className="bg-brand-950 border-white border-[1px] rounded-[18px] w-fit">
+        <div className="bg-brand-950 border-white border-[1px] rounded-[18px] ">
             <div className="border-b-white border-b-[1px] flex gap-2 ">
-                <h1 className="text-3xl font-bold pl-3 pt-2.5">{name}</h1>
+                <h1 className="text-3xl font-bold pl-3 pt-2.5 text-nowrap">{name}</h1>
                 {loan && <span className={"mt-3 text-3xl"}>|</span>}
                 {loan && <p title={loan.item.equipment.name} className="mt-4 text-2xl w-40 whitespace-nowrap overflow-hidden text-ellipsis">Unit {loan.item?.id}</p>}
                 {loan && <div className={"mt-4 mr-3 ml-auto rounded-md flex justify-center px-1 w-fit h-5 left-3" + (loan.status === "Returned" ? " bg-green-400" : loan.status === "Active" ? " bg-yellow-400" : " bg-red-600" )}>
@@ -78,7 +78,6 @@ export default function Card({ loan, user, setSelectedLoanId, setSideView }: Car
                 <button onClick={() => loan ? loan.delete() : user ? user.delete() : alert("Error")} className="button bg-red-600">Delete</button>
                 { loan && loan.status != "Returned" && <button onClick={() => loan.return()} className="button bg-green-500 ml-auto mr-3">Return</button>}
             </div>
-
         </div>
     )
 }
