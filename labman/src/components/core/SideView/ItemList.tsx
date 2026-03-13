6f9aeebcc98259769eaf7e56810b32b607bc8a45
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2026-03-13 16:51:29 +0100

    fix: loan status updates correctly in the UI
    Refs: #62

--- labman/src/components/core/SideView/ItemList.tsx
+++ labman/src/components/core/SideView/ItemList.tsx
@@ -12,20 +12,26 @@ type SelectableProps = BaseProps & {
      setSelectedUnit: (unit: Unit) => void;
 }
 
+type SelectableEditProps = BaseProps & {
+    variant: "selectableEdit";
+    selectedUnit: Unit | undefined;
+    setSelectedUnit: (unit: Unit) => void;
+}
+
 type editableProps = BaseProps & {
     variant: "editable";
     handleAddUnit: (name: string) => void;
     handleDeleteUnit: (id: number) => void;
  }
 
- type Props = SelectableProps | editableProps;
+ type Props = SelectableProps | editableProps | SelectableEditProps;
 
 export default function ItemList(props: Props) {
 
     // The initially selected unit has to persist between renders but uses to useEffect to update when the loan changes
     let selectedUnitRef : React.RefObject<number | undefined>;
 
-    if (props.variant === "selectable") {
+    if (props.variant === "selectableEdit") {
         selectedUnitRef = useRef(props.selectedUnit?.id);
 
         useEffect(() => {
@@ -42,7 +48,7 @@ export default function ItemList(props: Props) {
                 <div className="mt-2">
                     {props.equipmentData.items.map((unit, index) => (
                         <div key={unit.id} className="flex items-center justify-between bg-brand-200 rounded-md p-1 mb-3">
-                                <h1 className={ props.variant === "selectable" && unit.id === selectedUnitRef.current ? "font-bold text-xl text-blue-600" : "font-bold text-xl text-black"}>{unit.id}</h1>
+                                <h1 className={ props.variant === "selectableEdit" && unit.id === selectedUnitRef.current ? "font-bold text-xl text-blue-600" : "font-bold text-xl text-black"}>{unit.id}</h1>
                                 {(() => {
                                     switch (props.variant) {
                                         case "editable":
@@ -60,6 +66,17 @@ export default function ItemList(props: Props) {
                                         case "selectable":
                                             return (
                                                 <>
+                                                    { unit.activeLoan && (unit.activeLoan.status !== "Returned") && <h1 className="text-black font-bold">Borrowed</h1>}
+                                                    { (unit.activeLoan == null || unit.activeLoan.status === "Returned") && <button
+                                                        className="bg-white h-8 w-8 border-black border-1 rounded-full flex items-center justify-center"
+                                                        onClick={() => props.setSelectedUnit(unit)}>
+                                                        <div className={props.selectedUnit?.id == unit.id ? "bg-blue-600 h-4 w-4 rounded-full" : ""}></div>
+                                                    </button>}
+                                                </>
+                                            )
+                                        case "selectableEdit":
+                                            return (
+                                                <>
                                                     { unit.activeLoan && (unit.activeLoan.status !== "Returned" && unit.id !== selectedUnitRef.current) && <h1 className="text-black font-bold">Borrowed</h1>}
                                                     { (unit.activeLoan == null || unit.activeLoan.status === "Returned" || unit.id == selectedUnitRef.current) && <button
                                                         className="bg-white h-8 w-8 border-black border-1 rounded-full flex items-center justify-center"
@@ -67,6 +84,7 @@ export default function ItemList(props: Props) {
                                                         <div className={props.selectedUnit?.id == unit.id ? "bg-blue-600 h-4 w-4 rounded-full" : ""}></div>
                                                     </button>}
                                                 </>
+
                                             )
                                     }
                                 })()}
