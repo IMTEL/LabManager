71d275439aaff84052189b28930dedb82817ecde
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2026-04-21 15:36:31 +0200

    feat: migrate to vercel ai sdk
    change from a native mistral integration to using vercel ai sdk for a unified framework
    
    Refs: #87 #90

--- labman/src/lib/actions/aiActions.ts
+++ labman/src/lib/actions/aiActions.ts
@@ -1,36 +1,60 @@
 "use server"
 
-import {mistralClient} from "@/lib/mistral";
-import {MessageOutputEntry} from "@mistralai/mistralai/models/components";
+import prisma from "@/lib/prisma";
+import {generateText, Output} from 'ai'
+import {mistral} from '@ai-sdk/mistral'
+import {z} from "zod";
 
 type ActionResult<T> = | { type: "success"; data: T} | { type: "confirm"; message: string} | { type: "error"; message: string}
 
-interface Response {
-    categories: string[],
-    equipmentName : string
-}
 
 // TODO: This should also look through the database for relevant categories instead of always generating new ones
 export async function aiCategories(equipmentName : string) : Promise<ActionResult<string[]>> {
-     const messages = [
-     {"role":"user" as const, "content":equipmentName}];
 
-     const response = await mistralClient.beta.conversations.start({
-         agentId: 'ag_019d8b49ddbc7315bcc88f9d3df3cfed',
-         agentVersion: 9,
-         inputs: messages
-     })
+    const { output } = await generateText({
+        model: mistral("mistral-medium-latest"),
+        output: Output.object({
+            schema:z.object({
+                name: z.string(),
+                categories: z.array(z.string())
+            }),
+        }),
+        system: "You are an assistant in a system for managing an inventory of multiple types of equipment, this system is also used to lend the equipment out to borrowers and keep track of status, return dates etc.\n" +
+            "\n" +
+            "Your task is to find a maximun of five appropiate categories based on the name of the equipment that the user gives you. You will be given the name of an equipment and then you have to find possible categories appropiate for that equipment. In the case that you don't recieve any equipment name from the user leave both equipmentName and categories empty.\n" +
+            "\n" +
+            "The name of every category should have the first letter capitalized and have spaces between the words.",
+        prompt: equipmentName,
+    })
 
-    const outputs = response.outputs as MessageOutputEntry[]
-    const output = JSON.parse(outputs[0].content as string) as Response
+    console.log(output);
 
-     console.log(response);
-     console.log(output);
-     console.log(output.categories);
+
 
      if (output.categories && output.categories.length > 0) {
          return {type: "success", data: output.categories}
      } else {
          return {type: "error", message: "No categories found"}
      }
+ }
+
+ export async function aiTest(equipmentName : string) {
+
+     const { output } = await generateText({
+         model: mistral("mistral-medium-latest"),
+         output: Output.object({
+             schema:z.object({
+                 name: z.string(),
+                 categories: z.array(z.string())
+             }),
+         }),
+         system: "You are an assistant in a system for managing an inventory of multiple types of equipment, this system is also used to lend the equipment out to borrowers and keep track of status, return dates etc.\n" +
+             "\n" +
+             "Your task is to find a maximun of five appropiate categories based on the name of the equipment that the user gives you. You will be given the name of an equipment and then you have to find possible categories appropiate for that equipment. In the case that you don't recieve any equipment name from the user leave both equipmentName and categories empty.\n" +
+             "\n" +
+             "The name of every category should have the first letter capitalized.",
+         prompt: equipmentName,
+     })
+
+     console.log(output);
  }
