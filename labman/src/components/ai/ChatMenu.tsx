3bd1603bb14214d4e16d4627583548edc9a11a7c
Ola Bekkevold <ola.k.bekkevold@gmail.com>
2026-04-22 16:27:00 +0200

    feat: implement structure for chat interface
    the basic components which will be used to chat with the ai assistan are implemented and will be activated through the navbar
    
    Refs: #87 #89

---
+++ labman/src/components/ai/ChatMenu.tsx
@@ -0,0 +1,23 @@
+﻿import {loanCount} from "@/utils/inventoryUtils";
+
+interface ChatMenuProps {
+    setIsOpen: (isOpen: boolean) => void;
+}
+
+export default function ChatMenu({ setIsOpen }: ChatMenuProps) {
+
+    return (
+        <>
+            {/* Dark backdrop */}
+            <div
+                className="fixed inset-0 bg-black/70 z-40"
+                onClick={() => setIsOpen(false)}
+            />
+
+            {/* Right-side panel */}
+            <div className="fixed top-5 bottom-5 right-0 h-screen w-275 bg-brand-500 shadow-xl z-50 mr-5 rounded-lg flex flex-col ">
+                <div>Hello</div>
+            </div>
+        </>
+    )
+}
--- labman/src/components/core/NavBar.tsx
+++ labman/src/components/core/NavBar.tsx
@@ -2,22 +2,30 @@
 import PathName from "@/components/core/PathName";
 
 import {logout} from "@/lib/actions/userActions";
+import {useState} from "react";
+import ChatMenu from "@/components/ai/ChatMenu";
 
 export default function NavBar({ username }: { username: string | null }) {
+    const [isOpen, setIsOpen] = useState(false);
+
     async function logoutButton(){
         await logout();
-
     }
 
     return(
-        <div className="flex justify-between items-center border-b-white border-b-[1px] mb-5">
-            < PathName />
-            <div className="flex items-center">
-                <div className="mr-8 pt-4">
-                    <button className={"button bg-red-600"} onClick={logoutButton}>Logout</button>
+        <>
+            {isOpen && <ChatMenu setIsOpen={setIsOpen} />}
+            <div className="flex justify-between items-center border-b-white border-b-[1px] mb-5">
+                < PathName />
+                <div className="flex items-center">
+                    <div className="mr-8 pt-4">
+                        <button className={"button bg-green-600 mr-2"} onClick={() => setIsOpen(true)}>AI</button>
+                        <button className={"button bg-red-600"} onClick={logoutButton}>Logout</button>
+                    </div>
+                    <p className="pr-4 pt-7 font-bold text-2xl">{username || "Not logged in"}</p>
                 </div>
-                <p className="pr-4 pt-7 font-bold text-2xl">{username || "Not logged in"}</p>
             </div>
-        </div>
+        </>
+
     )
 }
